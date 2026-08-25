2dae7a2f968b9009d88d3b1732efaa68532e06f2
yeonu <146903038+2ewyeonwoo3@users.noreply.github.com>
2026-08-25 15:13:32 +0900

    [Passed] 합을 복사하기 (2ms, 0MB)

---
+++ trail1/합을 복사하기/copy-the-sum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a= 1, b = 2, c = 3;
+
+    a = b = c = a+b+c;
+
+    cout <<a << " ";
+    cout <<b << " ";
+    cout <<c;
+    return 0;
+}
