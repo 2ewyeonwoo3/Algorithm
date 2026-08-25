fc2e6c10549d047e65a8bad5b5f1c7518b79936d
yeonu <146903038+2ewyeonwoo3@users.noreply.github.com>
2026-08-25 14:03:39 +0900

    [Passed] 세 정수형 변수 선언 (2ms, 0MB)

---
+++ trail1/세 정수형 변수 선언/declaration-of-three-natural-numbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int a = 7;
+    int b=23;
+    int c = 30;
+
+    string d = " + ";
+    string e = " = ";
+
+    cout << a << d << b << e << c;
+
+
+    return 0;
+}
