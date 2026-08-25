bab977b5d9b5d86c1021ee536ac39efc7e48f0a3
yeonu <146903038+2ewyeonwoo3@users.noreply.github.com>
2026-08-25 17:05:41 +0900

    [Passed] 전화번호 바꾸기 (2ms, 0MB)

---
+++ trail1/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int a, x,y;
+    char c;
+    cin >> a>>c >> x >> c >> y;
+
+    cout << "010" <<c<<y<<c<<x;
+    return 0;
+}
